7443dcf515ff45dd99df4d8bae176be696f4dbc7
Aldo-Becker <113803505+Aldo-Becker@users.noreply.github.com>
2023-02-10 17:43:20 -0600

    Comprobación

--- Tareas_I_Parcial/Tareas_I_Parcial/Program.cs
+++ Tareas_I_Parcial/Tareas_I_Parcial/Program.cs
@@ -16,7 +16,7 @@ namespace Tareas_I_Parcial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Tarea2());
         }
     }
 }
