c5e0ef1f92e3b3e0a748adea3cb29b21774b4370
Aldo-Becker <113803505+Aldo-Becker@users.noreply.github.com>
2023-02-10 17:33:12 -0600

    Creación de la Tarea #2
    Realización de la Tarea #2

---
+++ Tareas_I_Parcial/Tareas_I_Parcial/Tarea2.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Tareas_I_Parcial
+{
+    partial class Tarea2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PromedioButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota 1er Parcial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota 2do Parcial:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota 3er Parcial:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota 4to Parcial:";
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota1TextBox.Location = new System.Drawing.Point(176, 107);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(134, 26);
+            this.Nota1TextBox.TabIndex = 4;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota2TextBox.Location = new System.Drawing.Point(176, 144);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(134, 26);
+            this.Nota2TextBox.TabIndex = 5;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota3TextBox.Location = new System.Drawing.Point(176, 180);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(134, 26);
+            this.Nota3TextBox.TabIndex = 6;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota4TextBox.Location = new System.Drawing.Point(176, 213);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(134, 26);
+            this.Nota4TextBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(710, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ingrese 4 Notas Para Determinar el Promedio que Obtuvo un Estudiante en la Clase";
+            // 
+            // PromedioButton
+            // 
+            this.PromedioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PromedioButton.Location = new System.Drawing.Point(471, 161);
+            this.PromedioButton.Name = "PromedioButton";
+            this.PromedioButton.Size = new System.Drawing.Size(128, 45);
+            this.PromedioButton.TabIndex = 9;
+            this.PromedioButton.Text = "Promedio";
+            this.PromedioButton.UseVisualStyleBackColor = true;
+            this.PromedioButton.Click += new System.EventHandler(this.PromedioButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(357, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(354, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Presione \"Promedio\" Para Ver el Resultado";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Tarea2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 390);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.PromedioButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Tarea2";
+            this.Text = "Tarea2";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Nota1TextBox;
+        private System.Windows.Forms.TextBox Nota2TextBox;
+        private System.Windows.Forms.TextBox Nota3TextBox;
+        private System.Windows.Forms.TextBox Nota4TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button PromedioButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Tareas_I_Parcial/Tareas_I_Parcial/Tarea2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tareas_I_Parcial
+{
+    public partial class Tarea2 : Form
+    {
+        public Tarea2()
+        {
+            InitializeComponent();
+        }
+
+        private async void PromedioButton_Click(object sender, EventArgs e)
+        {
+            if (Nota1TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota1TextBox, "Ingrese una Nota");
+                return;
+            }
+            if (Nota2TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota2TextBox, "Ingrese un Nota");
+                return;
+            }
+            if (Nota3TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota3TextBox, "Ingrese un Nota");
+                return;
+            }
+            if (Nota4TextBox.Text == "")
+            {
+                errorProvider1.SetError(Nota4TextBox, "Ingrese un Nota");
+                return;
+            }
+
+            errorProvider1.Clear();
+
+            decimal nota1 = Convert.ToDecimal(Nota1TextBox.Text);
+            decimal nota2 = Convert.ToDecimal(Nota2TextBox.Text);
+            decimal nota3 = Convert.ToDecimal(Nota3TextBox.Text);
+            decimal nota4 = Convert.ToDecimal(Nota4TextBox.Text);
+
+            decimal total = await PromedioAsync(nota1, nota2, nota3, nota4);
+
+            if (total >= 65)
+            {
+                MessageBox.Show($"El Promedio Final del Estudiante es {total}, Por lo Tanto el Estudiante Aprobó la Clase.");
+            }
+            else
+            {
+                MessageBox.Show($"El Promedio Final del Estudiante es {total}, Por lo Tanto el Estudiante Reprobó la Clase.");
+            }
+        }
+
+        private async Task<decimal> PromedioAsync(decimal n1, decimal n2, decimal n3, decimal n4)
+        {
+            decimal promedio = await Task.Run(() =>
+            {
+                return (n1 + n2 + n3 + n4) / 4;
+
+            });
+            return promedio;
+        }
+    }
+}
